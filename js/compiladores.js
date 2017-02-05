0da9c42327ed63eb83433ebf1d11ee6861f14ad1
CarlosNoriega <charlie02.06.93@gmail.com>
2017-02-04 21:49:01 -0600

    arreglo de palabras no aceptadas

--- js/compiladores.js
+++ js/compiladores.js
@@ -1,35 +1,98 @@
 
+function isWord(res){
+	console.log(res);
+	for(var i=0; i<res.length; i++){
+		if(!((res[i]>='A' && res[i]<='Z')||(res[i]>='a' && res[i]<='z'))){
+			return false;
+		}
+	}
+	return true;
+}
+
+function isNumber(res){
+	for(var i=0; i<res.length; i++){
+			if(!(res[i]>='0' && res[i]<='9')){
+			return false;
+		}
+	}
+	return true;
+}	
 
 function leer(){
 	var correcto=true;	
 	var tex=document.getElementById("texto").value;
 	console.log(tex.length);
 	var arr = [];
+	var res = tex.replace( /\n/g, " " ).split( " " );
 	var c=0;
-	//console.log(tex.length === 1 && tex.match(/[a-z]/i));
-	for (var i = 0; i <tex.length; i++) {
-		if (tex[i]>='A' && tex[i]<='Z') {
-			console.log(tex[i]+" si es letra");
-		}else if (tex[i]>='a' && tex[i]<='z') {
-			console.log(tex[i]+" si es letra");
-		}else if (tex[i]>='0' && tex[i]<='9') {
-			console.log(tex[i]+" si es letra");
-		}else if (tex[i]=='{') {
-			console.log(tex[i]+" si es aceptado");
-		}else if (tex[i]=='}') {
-			console.log(tex[i]+" si es aceptado");
-		}else if (tex[i]=='(') {
-			console.log(tex[i]+" si es aceptado");
-		}else if (tex[i]==')') {
-			console.log(tex[i]+" si es aceptado");
-		}else if (tex[i]=='|') {
-			console.log(tex[i]+" si es aceptado");
-		}else if (tex[i]==' ') {
-			console.log(tex[i]+" si es aceptado");
+	for(var i=0; i<res.length; i++){
+		if ((res[i][0]>='A' && res[i][0]<='Z')||(res[i][0]>='a' && res[i][0]<='z')) {
+			if (!isWord(res[i])) {
+				arr[c]=res[i];
+				c++;
+			}
+		}else if (res[i][0]>='0' && res[i][0]<='9') {
+			if (!isNumber(res[i])) {
+				arr[c]=res[i];
+				c++;
+			}
+
+		}else if (res[i][0]=='{') {
+			if(res[i].length>1){
+				var p=res[i];
+				delete p[0];
+				if (!isWord(p)) {
+					arr[c]=res[i];
+					c++;
+				}
+			}
+
+		}else if (res[i][0]=='}'){
+			if (res[i].length>1) {
+				arr[c]=res[i];
+				c++;
+			}
+
+		}else if (res[i][0]=='(') {
+			if(res[i].length>1){
+				var p=res[i];
+				p=p.substring(1,res[i].length);
+				
+				if (!isWord(p) && !isNumber(p)) {
+					if (!(p[0]=='(') && !(p[0]==')')) {
+						if (p[p.length-1]==')') {
+							p=p.substring(0,res[i].length-2);
+							console.log('palabra '+p);
+							if (!isWord(p) && !isNumber(p)) {
+								arr[c]=res[i];
+								c++;
+							}
+						}
+						else{
+							arr[c]=res[i];
+							c++;
+						}
+						
+					}
+				}
+			}
+
+		}else if (res[i][0]==')') {
+			if (res[i].length>1) {
+				if (res[i][0]=='{') {
+					arr[c]=res[i];
+					c++;
+				}
+			}
+
+		}else if (res[i][0]=='|') {
+			if (res[i].length>1) {
+				arr[c]=res[i];
+				c++;
+			}
+
 		}else{
-			console.log(tex[i]+" NO");
-			arr[c]=tex[i];
-			c++;
+
 		}
 	}
 	var t="";
